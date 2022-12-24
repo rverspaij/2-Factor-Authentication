b3de7195a8a5eab1b2a7a6fbb0f06ee2e3bb03c1
Raoul Verspaij <rnb1acc.verspaij@gmail.com>
2022-12-24 12:19:55 +0100

    package main
    import (
        "fmt"
        "net/http"
    )
    
    const htmlLogin =
    <html>
    <body>
        <form action="/login" method="POST">
            <label for="username">Username:</label><br>
            <input type="text" id="username" name="username"><br>
            <label for="password">Password:</label><br>
            <input type="password" id="password" name="password"><br><br>
            <input type="submit" value="Submit">
        </form>
    </body>
    </html>
    
    func login(w http.ResponseWriter, r *http.Request) {
        if r.Method == "POST" {
            // Get the username and password from the form
            username := r.FormValue("username")
            password := r.FormValue("password")
    
            checker := checkLogin(username, password)
            // Check the username and password against a database or other method
            if checker {
                // If the login is successful, redirect the user to the dashboard page
                http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
            } else {
                // If the login is unsuccessful, display an error message
                fmt.Fprint(w, "Incorrect username or password. Please try again.")
            }
        } else {
            // If the request is not a POST request, display the login form
            fmt.Fprint(w, htmlLogin)
        }
    }
    
    func checkLogin(username string, password string) (bool, error) {
        var correct bool
        correct = true
        return correct, nil
    
    }
    
    func main() {
        http.HandleFunc("/login", login)
        http.ListenAndServe(":8080", nil)
    }

--- main.go
+++ main.go
@@ -1,54 +1,127 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
-	"github.com/sethvargo/go-password/password"
-	gomail "gopkg.in/mail.v2"
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
+type DBConfig struct {
+	Server struct {
+		DbHost string `yaml:"host"`
+		DbPort string `yaml:"port"`
+	}
 
+	User struct {
+		DbUser     string `yaml:"user"`
+		DbPassword string `yaml:"password"`
+	} `yaml:"login"`
 }
 
-func readConfig() {
-
+type LoginValues struct {
+	Username string
+	Password string
 }
 
-func sendEmail(receiver string) {
-	m := gomail.NewMessage()
+const htmlLogin = `<html>
+<body>
+    <form action="/login" method="POST">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+</body>
+</html>`
 
-	// Set E-Mail sender
-	m.SetHeader("From", "from@gmail.com")
+func readConfig(cfg *DBConfig) error {
+	conf, err := os.ReadFile("conf.yml")
+	if err != nil {
+		return err
+	}
 
-	// Set E-Mail receivers
-	m.SetHeader("To", receiver)
+	err = yaml.Unmarshal([]byte(conf), &cfg)
+	if err != nil {
+		return err
+	}
+	if cfg.Server.DbHost == "" || cfg.Server.DbPort == "" || cfg.User.DbUser == "" || cfg.User.DbPassword == "" {
+		err := errors.New("empty fields")
+		return err
+	}
+	fmt.Println(cfg)
+	return err
+}
 
-	// Set E-Mail subject
-	m.SetHeader("Subject", "Verification code.")
+func connectDB(cfg *DBConfig) *sql.DB {
+	connection := cfg.User.DbUser + ":" + cfg.User.DbPassword + "@tcp(" + cfg.Server.DbHost + ":" + cfg.Server.DbPort + ")/test"
+	db, err := sql.Open("mysql", connection)
 
-	// Generate verification code
-	code, err := password.Generate(7, 7, 0, false, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Set E-Mail body. You can set plain text or html with text/html
-	m.SetBody("text/plain", code)
-
-	// Settings for SMTP server
-	d := gomail.NewDialer("smtp.gmail.com", 587, "from@gmail.com", "<email_password>")
-
-	// Now send E-Mail
-	if err := d.DialAndSend(m); err != nil {
-		fmt.Println(err)
-		panic(err)
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return
+	return db
 }
 
-//func generateCode() string {
+func createTable(db *sql.DB) error {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users (username VARCHAR(25) PRIMARY KEY NOT NULL, password VARCHAR(25) NOT NULL)")
+	return err
+}
 
-//}
+func (lv *LoginValues) login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// Get the username and password from the form
+		lv.Username = r.FormValue("username")
+		lv.Password = r.FormValue("password")
+
+		checker, err := checkLogin(lv.Username, lv.Password)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// Check the username and password against a database or other method
+		if checker {
+			http.Redirect(w, r, "/mfa", http.StatusSeeOther)
+		} else {
+			fmt.Fprint(w, "Incorrect username or password. Please try again.")
+		}
+	} else {
+		fmt.Fprint(w, htmlLogin)
+	}
+}
+
+func checkLogin(username string, password string) (bool, error) {
+	var correct bool
+	correct = true
+	return correct, nil
+
+}
+
+func newfunc(lv *LoginValues) {
+	fmt.Println(lv.Username)
+}
+
+func main() {
+	var config DBConfig
+	var loginValues LoginValues
+	readConfig(&config)
+	db := connectDB(&config)
+	err := createTable(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/login", loginValues.login)
+	http.ListenAndServe(":8080", nil)
+}
